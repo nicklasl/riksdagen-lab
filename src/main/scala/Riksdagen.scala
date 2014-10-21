f9ab9a0dfebb26efe5f79359b4200e9e6dcc5665
Nicklas Lundin <nicklaslundin@gmail.com>
2014-10-21 13:26:47 +0200

    some stuff for edges and vertices

--- src/main/scala/Riksdagen.scala
+++ src/main/scala/Riksdagen.scala
@@ -1,14 +1,10 @@
-import akka.actor.Props
 import play.api.libs.json.{JsArray, JsValue}
+import scala.collection.mutable
 import scala.concurrent.Await
 import scala.concurrent.duration.Duration
 import scala.concurrent.ExecutionContext.Implicits.global
-import ParseActor._
 
 object Riksdagen extends App {
-
-  val actorSystem = akka.actor.ActorSystem("ActorSystem")
-  val parseActor = actorSystem.actorOf(Props[ParseActor], name = "ParseActor")
 
   val baseUri = "http://data.riksdagen.se/dokumentlista/?sok=&doktyp=mot&sort=datum&sortorder=desc&utformat=json&a=s"
 
@@ -21,6 +17,9 @@ object Riksdagen extends App {
     1
   })
 
+  val vertices = new mutable.HashMap[String, String]
+  val edges = new mutable.HashMap[(String, String), Int]
+
   val builder = new com.ning.http.client.AsyncHttpClientConfig.Builder()
   val client = new play.api.libs.ws.ning.NingWSClient(builder.build())
 
@@ -28,13 +27,41 @@ object Riksdagen extends App {
     client.url(uri(page)).get().map {
       response =>
         val docs: Seq[JsValue] = ((response.json \ "dokumentlista") \ "dokument").as[JsArray].value
-        parseActor ! DocsToParse(docs)
+        val list: List[String] = docs.map {
+          json =>
+            val intressenter = {
+              val value: JsValue = json \ "dokintressent" \ "intressent"
+              if (value.isInstanceOf[JsArray]) value.as[JsArray].value
+              else Seq(value)
+            }
+            if (intressenter.size > 1) {
+              intressenter.foreach {
+                intressent =>
+                  def intressentId(intressent: JsValue): String = (intressent \ "intressent_id").as[String]
+                  vertices.put(intressentId(intressent), (intressent \ "namn").as[String])
+                  intressenter.foreach {
+                    intressent2 =>
+                      val key: (String, String) = (intressentId(intressent), intressentId(intressent2))
+                      val hitOption = edges.get(key)
+                      if(hitOption.isDefined) edges.update(key, hitOption.get + 1)
+                      else edges.put(key, 1)
+                  }
+
+              }
+            }
+            s"klar med $page"
+        }.toList
+        list.foreach(println(_))
     }
   }
-  seq.foreach(fut => Await.ready(fut, Duration("5 seconds")))
-
   println(s"waiting for ${seq.size} request(s) to finish")
-  actorSystem.stop(parseActor)
-  client.close()
+  seq.foreach(fut => Await.ready(fut, Duration("15 seconds")))
 
+  println("VERTICES!")
+  println(vertices)
+
+  println("EDGES!")
+  println(edges.filter(x => x._2 > 1))
+
+  client.close()
 }
